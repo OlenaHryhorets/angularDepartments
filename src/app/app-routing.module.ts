9fcebd9f0720d205905e4153b3d4de7ba72877d8
lena <adamenkolena7@gmail.com>
2020-06-09 10:33:16 +0300

    Routing module add

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
+  { path: 'departments', loadChildren: () => import('./departments/departments.module').then(m => m.DepartmentsModule) }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
